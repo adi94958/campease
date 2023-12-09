2def51861892de8ee8a9ba1ff00d44904b673ca0
adrianeka <78060253+adrianeka@users.noreply.github.com>
2023-12-09 23:38:15 +0700

    Update KavlingExport.php

--- Laravel-9-AdminLTE-master/app/Exports/KavlingExport.php
+++ Laravel-9-AdminLTE-master/app/Exports/KavlingExport.php
@@ -6,14 +6,39 @@ use App\Models\Kavling;
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromQuery;
 use Maatwebsite\Excel\Concerns\Exportable;
-use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class KavlingExport implements FromQuery
+class KavlingExport implements FromQuery, WithHeadings, ShouldAutoSize, WithStyles
 {
     use Exportable;
 
     public function query()
     {
         return Kavling::query();
+    }
+
+    public function headings(): array
+    {
+        // Menentukan header kolom
+        return [
+            'No',
+            'Nama Kavling',
+            'Harga Kavling',
+            'Status Kavling',
+            'Created At',
+            'Updated At',
+        ];
+    }
+
+    public function styles(Worksheet $sheet)
+    {
+        // Menambahkan border ke semua sel
+        $sheet->getStyle('A1:' . $sheet->getHighestColumn() . $sheet->getHighestRow())
+            ->getBorders()
+            ->getAllBorders()
+            ->setBorderStyle('thin');
     }
 }
