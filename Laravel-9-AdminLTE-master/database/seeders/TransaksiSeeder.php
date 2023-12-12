b632e779ff0bb800978949e96ec8ce7f128b61b5
Muhammad Ikhsan Maulana Taqwim JTK D3 1B <117416421+Ikhsan310105@users.noreply.github.com>
2023-12-12 22:38:03 +0700

    seed new

--- Laravel-9-AdminLTE-master/database/seeders/TransaksiSeeder.php
+++ Laravel-9-AdminLTE-master/database/seeders/TransaksiSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\transaksi;
 use Faker\Factory as Faker;
+use Illuminate\Support\Facades\DB;
 
 class TransaksiSeeder extends Seeder
 {
@@ -26,13 +27,26 @@ class TransaksiSeeder extends Seeder
         }, $kavlingNumbers);
 
         foreach (range(1, 1000) as $index) {
+            $harga = DB::table('data_kavling')
+                ->where('area_kavling', $kavlingNumbers[$index - 1])
+                ->value('harga');
+
+            // Calculate the number of days between check-in and check-out
+            $checkIn = now()->subMonth()->subDays(rand(1, 20));
+            $checkOut = now()->addDays(rand(1, 7));
+            $numberOfDays = $checkOut->diffInDays($checkIn);
+
+            // Calculate the harga by multiplying with the number of days
+            $totalHarga = $harga * $numberOfDays;
+
             Transaksi::create([
                 'id' => $index,
                 'nama_penyewa' => $namaDepan[array_rand($namaDepan)] . ' ' . $namaBelakang[array_rand($namaBelakang)],
                 'no_handphone' => '08' . $faker->randomNumber(8),
-                'tanggal_check_in' => now()->subMonth()->subDays(rand(1, 30)),
-                'tanggal_check_out' => now()->addDays(rand(1, 7)),
+                'tanggal_check_in' => $checkIn,
+                'tanggal_check_out' => $checkOut,
                 'area_kavling' => $kavlingNumbers[$index - 1],
+                'harga' => $totalHarga,
                 'created_at' => now(),
                 'updated_at' => now(),
             ]);
