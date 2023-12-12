79cbfd3e5c212f93a0d100a9f281ec697d6ab9ae
adrianeka <78060253+adrianeka@users.noreply.github.com>
2023-12-12 21:06:58 +0700

    Fix FasilitasExport dan FeedbackExport

--- Laravel-9-AdminLTE-master/app/Exports/FasilitasExport.php
+++ Laravel-9-AdminLTE-master/app/Exports/FasilitasExport.php
@@ -5,13 +5,38 @@ namespace App\Exports;
 use App\Models\Fasilitas;
 use Maatwebsite\Excel\Concerns\FromQuery;
 use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class FasilitasExport implements FromQuery
+class FasilitasExport implements FromQuery, WithHeadings, ShouldAutoSize, WithStyles
 {
     use Exportable;
 
     public function query()
     {
         return Fasilitas::query();
+    }
+
+    public function headings(): array
+    {
+        // Menentukan header kolom
+        return [
+            'No',
+            'Nama Fasilitas',
+            'Jumlah',
+            'Created At',
+            'Updated At',
+        ];
+    }
+
+    public function styles(Worksheet $sheet)
+    {
+        // Menambahkan border ke semua sel
+        $sheet->getStyle('A1:' . $sheet->getHighestColumn() . $sheet->getHighestRow())
+            ->getBorders()
+            ->getAllBorders()
+            ->setBorderStyle('thin');
     }
 }
--- Laravel-9-AdminLTE-master/app/Exports/FeedbackExport.php
+++ Laravel-9-AdminLTE-master/app/Exports/FeedbackExport.php
@@ -3,15 +3,42 @@
 namespace App\Exports;
 
 use App\Models\Feedback;
+use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromQuery;
 use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class FeedbackExport implements FromQuery
+class FeedbackExport implements FromQuery, WithHeadings, ShouldAutoSize, WithStyles
 {
     use Exportable;
 
     public function query()
     {
         return Feedback::query();
+    }
+
+    public function headings(): array
+    {
+        // Menentukan header kolom
+        return [
+            'No',
+            'Id Pengirim',
+            'Isi Feedback',
+            'Rating Feedback',
+            'Created At',
+            'Updated At',
+        ];
+    }
+
+    public function styles(Worksheet $sheet)
+    {
+        // Menambahkan border ke semua sel
+        $sheet->getStyle('A1:' . $sheet->getHighestColumn() . $sheet->getHighestRow())
+            ->getBorders()
+            ->getAllBorders()
+            ->setBorderStyle('thin');
     }
 }
